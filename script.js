c06f67e133705121da1262ef619b1e43e64a1605
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-25 15:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
+	const table = document.getElementById('sampleTable');
+	const topRow = table.insertRow(0);
+	const cell1 = topRow.insertCell(0);
+	const cell2 = topRow.insertCell(1);
+	cell1.textContent = "New Cell1";
+	cell2.textContent = "New Cell2"
   
   
 }
